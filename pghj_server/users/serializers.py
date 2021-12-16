b4e527954c318847f98edce82028f50cf13ebc71
apphia39 <apphia39@naver.com>
2021-12-16 23:54:32 +0900

    v1: api server
    [유저기능] 회원가입 | 로그인 | 로그아웃 | 히스토리
    [파일기능] 이미지업로드 | ppt 생성 | ppt 다운로드

---
+++ pghj_server/users/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            user_id = validated_data['user_id'],
+            password = validated_data['password'],
+            user_name = validated_data['user_name'],
+        )
+        return user
+
+    class Meta:
+        model = User
+        fields = ('user_id', 'password', 'user_name')
